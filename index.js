a5d0f0d00e528309c3fb065657b7c9bd721fc93b
Ruben Schaller <rubenusshd@gmail.com>
2021-12-18 13:45:19 +0200

    test

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', function(req, res) {
 
 app.get('/location/:id', (req, res) => {
     console.log(req.params.id);
-    res.json({ background: 'orange' })
+    res.json({ background: 'red' })
     })
 
 const PORT = process.env.PORT || 8080;
